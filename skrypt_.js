ec77bc2c5921a451e983d7f3d5f75670fe746cab
Terminator056 <75201110+Terminator056@users.noreply.github.com>
2021-01-29 08:42:20 +0200

    Add files via upload

---
+++ skrypt_.js
@@ -0,0 +1,676 @@
+const petsData = [
+   {
+    name: "Astra ***",
+    panstwo: "Polska",
+    miasto: ["Kraków", "Białystok", "<strong>(Najwiękcej gwiazdek)</strong> Warszawa"],
+    rok_budowy: 2016,
+    photo: "https://github.com/Terminator056/JSON-projekt-mobine/blob/main/obrazki/1.jpg?raw=true",
+    jedzenie: ["bez","poranek, kolacja","3 razy dziennie"],
+    typ_oplaty : "karta/gotowka",
+    ulica : "Centralna",
+    zwierzeta : "koty",
+    variacje: {
+            ilosc_pokoji: {
+              "od" : 1,
+              "do" : 10
+            },
+            typ_pokoju:["jedno osobowy","dwu osobowy", "4 osobowy","20 osobowy"],
+            odleglosc_do_centrum: {
+              "od" : 10,
+              "do" : 50
+            },
+            odlegolosc_do_lotniska: {
+              "od" : 10,
+              "do" : 50
+            },
+            typ_osoby : "Dorosly/dziecko",
+            odleglosc_do_plazy: {
+              "od" : 10,
+              "do" : 50
+            },
+            odleglosc_do_muzeum: {
+              "od" : 10,
+              "do" : 50
+            },
+            basejn: ["jest","nie"],
+            miesjsce_samochodu: ["jest","nie"],
+            ilosc_basejnów: ["1","2"],
+            ilosc_kawiarni: ["1","2"],
+            wifi:["jest","nie"],
+            spa: ["jest","nie"],
+            silownia: ["jest","nie"],
+            data_wynajecia : {
+            "od" : "2021-01-01",
+            "do" : "2022-01-01"
+            },
+            data_wyjscia : {
+              "od" : "2021-01-01",
+              "do" : "2022-01-01"
+            },
+            cena : {
+              "od" : 10,
+              "do" : 50
+            }
+          }
+   
+  },
+
+  {
+    name: "Mandaryna ***",
+    panstwo: "Anglia",
+    rok_budowy: 2008,
+    photo: "https://github.com/Terminator056/JSON-projekt-mobine/blob/main/obrazki/10.jpg?raw=true",
+    jedzenie: ["bez","poranek, kolacja","3 razy dziennie"],
+    typ_oplaty : "karta/gotowka",
+    ulica : "Centralna",
+    zwierzeta : "pieski",
+    variacje: {
+            ilosc_pokoji: {
+              "od" : 1,
+              "do" : 10
+            },
+            typ_pokoju:["jedno osobowy","dwu osobowy", "4 osobowy","20 osobowy"],
+            odleglosc_do_centrum: {
+              "od" : 10,
+              "do" : 50
+            },
+            odlegolosc_do_lotniska: {
+              "od" : 10,
+              "do" : 50
+            },
+            typ_osoby : "Dorosly/dziecko",
+            odleglosc_do_plazy: {
+              "od" : 10,
+              "do" : 50
+            },
+            odleglosc_do_muzeum: {
+              "od" : 10,
+              "do" : 50
+            },
+            basejn: ["jest","nie"],
+            miesjsce_samochodu: ["jest","nie"],
+            ilosc_basejnów: ["1","2"],
+            ilosc_kawiarni: ["1","2"],
+            wifi:["jest","nie"],
+            spa: ["jest","nie"],
+            silownia: ["jest","nie"],
+            data_wynajecia : {
+            "od" : "2021-01-01",
+            "do" : "2022-01-01"
+            },
+            data_wyjscia : {
+              "od" : "2021-01-01",
+              "do" : "2022-01-01"
+            },
+            cena : {
+              "od" : 10,
+              "do" : 50
+            }
+          }
+  },
+
+  {
+    name: "Ava **",
+    panstwo: "Litwa",
+    miasto: ["Wilno", "Szwentoji", "Palanga"],
+    rok_budowy: 2012,
+    photo: "https://github.com/Terminator056/JSON-projekt-mobine/blob/main/obrazki/11.jpg?raw=true",
+   jedzenie: ["bez","poranek, kolacja","3 razy dziennie"],
+    typ_oplaty : "karta/gotowka",
+    ulica : "Centralna",
+    zwierzeta : "małpy",
+    variacje: {
+            ilosc_pokoji: {
+              "od" : 1,
+              "do" : 10
+            },
+            typ_pokoju:["jedno osobowy","dwu osobowy", "4 osobowy","20 osobowy"],
+            odleglosc_do_centrum: {
+              "od" : 10,
+              "do" : 50
+            },
+            odlegolosc_do_lotniska: {
+              "od" : 10,
+              "do" : 50
+            },
+            typ_osoby : "Dorosly/dziecko",
+            odleglosc_do_plazy: {
+              "od" : 10,
+              "do" : 50
+            },
+            odleglosc_do_muzeum: {
+              "od" : 10,
+              "do" : 50
+            },
+            basejn: ["jest","nie"],
+            miesjsce_samochodu: ["jest","nie"],
+            ilosc_basejnów: ["1","2"],
+            ilosc_kawiarni: ["1","2"],
+            wifi:["jest","nie"],
+            spa: ["jest","nie"],
+            silownia: ["jest","nie"],
+            data_wynajecia : {
+            "od" : "2021-01-01",
+            "do" : "2022-01-01"
+            },
+            data_wyjscia : {
+              "od" : "2021-01-01",
+              "do" : "2022-01-01"
+            },
+            cena : {
+              "od" : 10,
+              "do" : 50
+            }
+          }
+  },
+
+  {
+    name: "Posejdon ****",
+    panstwo: "Szwecja",
+    miasto: ["Stokholm", "Holmstock", "Kikimara"],
+    rok_budowy: 2010,
+    photo: "https://github.com/Terminator056/JSON-projekt-mobine/blob/main/obrazki/12.jpg?raw=true",
+    jedzenie: ["bez","poranek, kolacja","3 razy dziennie"],
+    typ_oplaty : "karta/gotowka",
+    ulica : "Centralna",
+    zwierzeta : "żmije",
+    variacje: {
+            ilosc_pokoji: {
+              "od" : 1,
+              "do" : 10
+            },
+            typ_pokoju:["jedno osobowy","dwu osobowy", "4 osobowy","20 osobowy"],
+            odleglosc_do_centrum: {
+              "od" : 10,
+              "do" : 50
+            },
+            odlegolosc_do_lotniska: {
+              "od" : 10,
+              "do" : 50
+            },
+            typ_osoby : "Dorosly/dziecko",
+            odleglosc_do_plazy: {
+              "od" : 10,
+              "do" : 50
+            },
+            odleglosc_do_muzeum: {
+              "od" : 10,
+              "do" : 50
+            },
+            basejn: ["jest","nie"],
+            miesjsce_samochodu: ["jest","nie"],
+            ilosc_basejnów: ["1","2"],
+            ilosc_kawiarni: ["1","2"],
+            wifi:["jest","nie"],
+            spa: ["jest","nie"],
+            silownia: ["jest","nie"],
+            data_wynajecia : {
+            "od" : "2021-01-01",
+            "do" : "2022-01-01"
+            },
+            data_wyjscia : {
+              "od" : "2021-01-01",
+              "do" : "2022-01-01"
+            },
+            cena : {
+              "od" : 10,
+              "do" : 50
+            }
+          }
+  },
+
+  {
+    name: "Octupus ****",
+    panstwo: "Szwecja",
+    miasto: ["Stokholm", "Holmstock", "Kikimara"],
+    rok_budowy: 2010,
+    photo: "https://github.com/Terminator056/JSON-projekt-mobine/blob/main/obrazki/13.jpg?raw=true",
+    jedzenie: ["bez","poranek, kolacja","3 razy dziennie"],
+    typ_oplaty : "karta/gotowka",
+    ulica : "Centralna",
+    zwierzeta : "chupacabry",
+    variacje: {
+            ilosc_pokoji: {
+              "od" : 1,
+              "do" : 10
+            },
+            typ_pokoju:["jedno osobowy","dwu osobowy", "4 osobowy","20 osobowy"],
+            odleglosc_do_centrum: {
+              "od" : 10,
+              "do" : 50
+            },
+            odlegolosc_do_lotniska: {
+              "od" : 10,
+              "do" : 50
+            },
+            typ_osoby : "Dorosly/dziecko",
+            odleglosc_do_plazy: {
+              "od" : 10,
+              "do" : 50
+            },
+            odleglosc_do_muzeum: {
+              "od" : 10,
+              "do" : 50
+            },
+            basejn: ["jest","nie"],
+            miesjsce_samochodu: ["jest","nie"],
+            ilosc_basejnów: ["1","2"],
+            ilosc_kawiarni: ["1","2"],
+            wifi:["jest","nie"],
+            spa: ["jest","nie"],
+            silownia: ["jest","nie"],
+            data_wynajecia : {
+            "od" : "2021-01-01",
+            "do" : "2022-01-01"
+            },
+            data_wyjscia : {
+              "od" : "2021-01-01",
+              "do" : "2022-01-01"
+            },
+            cena : {
+              "od" : 10,
+              "do" : 50
+            }
+          }
+  },
+
+  {
+    name: "Jack ****",
+    panstwo: "Szwecja",
+    miasto: ["Stokholm", "Holmstock", "Kikimara"],
+    rok_budowy: 2010,
+    photo: "https://github.com/Terminator056/JSON-projekt-mobine/blob/main/obrazki/14.jpg?raw=true",
+    jedzenie: ["bez","poranek, kolacja","3 razy dziennie"],
+    typ_oplaty : "karta/gotowka",
+    ulica : "Centralna",
+    zwierzeta : "lwy",
+    variacje: {
+            ilosc_pokoji: {
+              "od" : 1,
+              "do" : 10
+            },
+            typ_pokoju:["jedno osobowy","dwu osobowy", "4 osobowy","20 osobowy"],
+            odleglosc_do_centrum: {
+              "od" : 10,
+              "do" : 50
+            },
+            odlegolosc_do_lotniska: {
+              "od" : 10,
+              "do" : 50
+            },
+            typ_osoby : "Dorosly/dziecko",
+            odleglosc_do_plazy: {
+              "od" : 10,
+              "do" : 50
+            },
+            odleglosc_do_muzeum: {
+              "od" : 10,
+              "do" : 50
+            },
+            basejn: ["jest","nie"],
+            miesjsce_samochodu: ["jest","nie"],
+            ilosc_basejnów: ["1","2"],
+            ilosc_kawiarni: ["1","2"],
+            wifi:["jest","nie"],
+            spa: ["jest","nie"],
+            silownia: ["jest","nie"],
+            data_wynajecia : {
+            "od" : "2021-01-01",
+            "do" : "2022-01-01"
+            },
+            data_wyjscia : {
+              "od" : "2021-01-01",
+              "do" : "2022-01-01"
+            },
+            cena : {
+              "od" : 10,
+              "do" : 50
+            }
+          }
+  },
+
+  {
+    name: "King ****",
+    panstwo: "Szwecja",
+    miasto: ["Stokholm", "Holmstock", "Kikimara"],
+    rok_budowy: 2010,
+    photo: "https://github.com/Terminator056/JSON-projekt-mobine/blob/main/obrazki/2.jpg?raw=true",
+    jedzenie: ["bez","poranek, kolacja","3 razy dziennie"],
+    typ_oplaty : "karta/gotowka",
+    ulica : "Centralna",
+    zwierzeta : "wieloryby",
+    variacje: {
+            ilosc_pokoji: {
+              "od" : 1,
+              "do" : 10
+            },
+            typ_pokoju:["jedno osobowy","dwu osobowy", "4 osobowy","20 osobowy"],
+            odleglosc_do_centrum: {
+              "od" : 10,
+              "do" : 50
+            },
+            odlegolosc_do_lotniska: {
+              "od" : 10,
+              "do" : 50
+            },
+            typ_osoby : "Dorosly/dziecko",
+            odleglosc_do_plazy: {
+              "od" : 10,
+              "do" : 50
+            },
+            odleglosc_do_muzeum: {
+              "od" : 10,
+              "do" : 50
+            },
+            basejn: ["jest","nie"],
+            miesjsce_samochodu: ["jest","nie"],
+            ilosc_basejnów: ["1","2"],
+            ilosc_kawiarni: ["1","2"],
+            wifi:["jest","nie"],
+            spa: ["jest","nie"],
+            silownia: ["jest","nie"],
+            data_wynajecia : {
+            "od" : "2021-01-01",
+            "do" : "2022-01-01"
+            },
+            data_wyjscia : {
+              "od" : "2021-01-01",
+              "do" : "2022-01-01"
+            },
+            cena : {
+              "od" : 10,
+              "do" : 50
+            }
+          }
+  },
+
+  {
+    name: "Xana ****",
+    panstwo: "Szwecja",
+    miasto: ["Stokholm", "Holmstock", "Kikimara"],
+    rok_budowy: 2010,
+    photo: "https://github.com/Terminator056/JSON-projekt-mobine/blob/main/obrazki/3.jpg?raw=true",
+    jedzenie: ["bez","poranek, kolacja","3 razy dziennie"],
+    typ_oplaty : "karta/gotowka",
+    ulica : "Centralna",
+    zwierzeta : "wierblata",
+    variacje: {
+            ilosc_pokoji: {
+              "od" : 1,
+              "do" : 10
+            },
+            typ_pokoju:["jedno osobowy","dwu osobowy", "4 osobowy","20 osobowy"],
+            odleglosc_do_centrum: {
+              "od" : 10,
+              "do" : 50
+            },
+            odlegolosc_do_lotniska: {
+              "od" : 10,
+              "do" : 50
+            },
+            typ_osoby : "Dorosly/dziecko",
+            odleglosc_do_plazy: {
+              "od" : 10,
+              "do" : 50
+            },
+            odleglosc_do_muzeum: {
+              "od" : 10,
+              "do" : 50
+            },
+            basejn: ["jest","nie"],
+            miesjsce_samochodu: ["jest","nie"],
+            ilosc_basejnów: ["1","2"],
+            ilosc_kawiarni: ["1","2"],
+            wifi:["jest","nie"],
+            spa: ["jest","nie"],
+            silownia: ["jest","nie"],
+            data_wynajecia : {
+            "od" : "2021-01-01",
+            "do" : "2022-01-01"
+            },
+            data_wyjscia : {
+              "od" : "2021-01-01",
+              "do" : "2022-01-01"
+            },
+            cena : {
+              "od" : 10,
+              "do" : 50
+            }
+          }
+  },
+
+  {
+    name: "Lazarus ****",
+    panstwo: "Szwecja",
+    miasto: ["Stokholm", "Holmstock", "Kikimara"],
+    rok_budowy: 2010,
+    photo: "https://github.com/Terminator056/JSON-projekt-mobine/blob/main/obrazki/4.jpg?raw=true",
+    jedzenie: ["bez","poranek, kolacja","3 razy dziennie"],
+    typ_oplaty : "karta/gotowka",
+    ulica : "Centralna",
+    zwierzeta : "konie",
+    variacje: {
+            ilosc_pokoji: {
+              "od" : 1,
+              "do" : 10
+            },
+            typ_pokoju:["jedno osobowy","dwu osobowy", "4 osobowy","20 osobowy"],
+            odleglosc_do_centrum: {
+              "od" : 10,
+              "do" : 50
+            },
+            odlegolosc_do_lotniska: {
+              "od" : 10,
+              "do" : 50
+            },
+            typ_osoby : "Dorosly/dziecko",
+            odleglosc_do_plazy: {
+              "od" : 10,
+              "do" : 50
+            },
+            odleglosc_do_muzeum: {
+              "od" : 10,
+              "do" : 50
+            },
+            basejn: ["jest","nie"],
+            miesjsce_samochodu: ["jest","nie"],
+            ilosc_basejnów: ["1","2"],
+            ilosc_kawiarni: ["1","2"],
+            wifi:["jest","nie"],
+            spa: ["jest","nie"],
+            silownia: ["jest","nie"],
+            data_wynajecia : {
+            "od" : "2021-01-01",
+            "do" : "2022-01-01"
+            },
+            data_wyjscia : {
+              "od" : "2021-01-01",
+              "do" : "2022-01-01"
+            },
+            cena : {
+              "od" : 10,
+              "do" : 50
+            }
+          }
+  },
+
+  {
+    name: "Ola ****",
+    panstwo: "Szwecja",
+    miasto: ["Stokholm", "Holmstock", "Kikimara"],
+    rok_budowy: 2010,
+    photo: "https://github.com/Terminator056/JSON-projekt-mobine/blob/main/obrazki/5.jpg?raw=true",
+    jedzenie: ["bez","poranek, kolacja","3 razy dziennie"],
+    typ_oplaty : "karta/gotowka",
+    ulica : "Centralna",
+    zwierzeta : "leopardy",
+    variacje: {
+            ilosc_pokoji: {
+              "od" : 1,
+              "do" : 10
+            },
+            typ_pokoju:["jedno osobowy","dwu osobowy", "4 osobowy","20 osobowy"],
+            odleglosc_do_centrum: {
+              "od" : 10,
+              "do" : 50
+            },
+            odlegolosc_do_lotniska: {
+              "od" : 10,
+              "do" : 50
+            },
+            typ_osoby : "Dorosly/dziecko",
+            odleglosc_do_plazy: {
+              "od" : 10,
+              "do" : 50
+            },
+            odleglosc_do_muzeum: {
+              "od" : 10,
+              "do" : 50
+            },
+            basejn: ["jest","nie"],
+            miesjsce_samochodu: ["jest","nie"],
+            ilosc_basejnów: ["1","2"],
+            ilosc_kawiarni: ["1","2"],
+            wifi:["jest","nie"],
+            spa: ["jest","nie"],
+            silownia: ["jest","nie"],
+            data_wynajecia : {
+            "od" : "2021-01-01",
+            "do" : "2022-01-01"
+            },
+            data_wyjscia : {
+              "od" : "2021-01-01",
+              "do" : "2022-01-01"
+            },
+            cena : {
+              "od" : 10,
+              "do" : 50
+            }
+          }
+  },
+
+  {
+    name: "Kraken ****",
+    panstwo: "Szwecja",
+    miasto: ["Stokholm", "Holmstock", "Kikimara"],
+    rok_budowy: 2010,
+    photo: "https://github.com/Terminator056/JSON-projekt-mobine/blob/main/obrazki/6.jpg?raw=true",
+    jedzenie: ["bez","poranek, kolacja","3 razy dziennie"],
+    typ_oplaty : "karta/gotowka",
+    ulica : "Centralna",
+    zwierzeta : "czlowiek sniezny",
+    variacje: {
+            ilosc_pokoji: {
+              "od" : 1,
+              "do" : 10
+            },
+            typ_pokoju:["jedno osobowy","dwu osobowy", "4 osobowy","20 osobowy"],
+            odleglosc_do_centrum: {
+              "od" : 10,
+              "do" : 50
+            },
+            odlegolosc_do_lotniska: {
+              "od" : 10,
+              "do" : 50
+            },
+            typ_osoby : "Dorosly/dziecko",
+            odleglosc_do_plazy: {
+              "od" : 10,
+              "do" : 50
+            },
+            odleglosc_do_muzeum: {
+              "od" : 10,
+              "do" : 50
+            },
+            basejn: ["jest","nie"],
+            miesjsce_samochodu: ["jest","nie"],
+            ilosc_basejnów: ["1","2"],
+            ilosc_kawiarni: ["1","2"],
+            wifi:["jest","nie"],
+            spa: ["jest","nie"],
+            silownia: ["jest","nie"],
+            data_wynajecia : {
+            "od" : "2021-01-01",
+            "do" : "2022-01-01"
+            },
+            data_wyjscia : {
+              "od" : "2021-01-01",
+              "do" : "2022-01-01"
+            },
+            cena : {
+              "od" : 10,
+              "do" : 50
+            }
+          }
+  }
+];
+
+
+
+function age(rok_budowy) {
+  let calculatedAge = new Date().getFullYear() - rok_budowy;
+  if (calculatedAge == 1) {
+    return "Tylko ma 1 rok po budowie";
+  } else if (calculatedAge == 0) {
+    return "Budynek ma do roku jak został zbudowany";
+  } else {
+    return `${calculatedAge} lat budynkowi`;
+  }
+}
+
+function foods(foods) {
+  return `
+<h4>Dostępne miasta hoteli</h4>
+<ul class="foods-list">
+${foods.map(food => `<li>${food}</li>`).join("")}
+</ul>
+`;
+}
+
+function petTemplate(pet) {
+  return `
+    <div class="animal">
+    <img class="pet-photo" src="${pet.photo}">
+    <h2 class="pet-name">${pet.name} <span class=panstwo>(${pet.panstwo})</span></h2>
+    <p><strong>Ilość lat budynkowi:</strong> ${age(pet.rok_budowy)}</p>
+    ${pet.miasto ? foods(pet.miasto) : ""}
+    </div>
+  `;
+}
+
+function petTemplate1(pet) {
+  return `
+    <div class="container" >
+    <table>
+    <thread>
+    <tr>
+    <th ><h2 >${pet.name} <span >(${pet.panstwo})</span></h2></th>
+    </tr>
+    </table>
+    </thread>
+    
+    </div>
+  `;
+}
+
+
+/*
+function myFunction() {
+  document.getElementById("tabelka").innerHTML = "Hello World";
+}
+*/
+function changeContent() {
+document.getElementById("tabelka").innerHTML = 
+  `
+  <h1 class="app-title">Mamy (${petsData.length} hoteli)</h1>
+  ${petsData.map(petTemplate).join("")}
+  <p class="footer">Te ${petsData.length} hotele zostały dodane ostatnio. Wróć i zobacz więcej.</p>
+`;
+
+}
+
+document.getElementById("tabelka").innerHTML = `
+  
+  ${petsData.map(petTemplate1).join("")}
+  <p class="footer">Te ${petsData.length} hotele zostały dodane ostatnio. Wróć i zobacz więcej.</p>
+`;
